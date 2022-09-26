3d6375d4be9a3147ee4aea7d667f60039a1d6980
Pankaj-Mali <malipankaj660@gmail.com>
2022-09-26 20:35:43 +0530

    components , style , packeges

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import Views from './components/view';
+import Landing from './components/landing';
+import Post from './components/post';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+   <BrowserRouter>
+     <Routes>
+       <Route path='/' element={<Landing/>}/>
+       <Route path='/postview' element={<Views/>}/>
+       <Route path="/addPost" element={<Post/>} />
+     </Routes>
+   </BrowserRouter>
   );
 }
 
