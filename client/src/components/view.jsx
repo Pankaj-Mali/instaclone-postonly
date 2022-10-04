7de51046f2421ece63ce256216832acd09516fdb
Pankaj-Mali <malipankaj660@gmail.com>
2022-10-04 20:59:29 +0530

    home page comlite

--- client/src/components/view.jsx
+++ client/src/components/view.jsx
@@ -4,20 +4,25 @@ import Header from "./header"
 import { useState } from "react"
 import { useEffect } from "react"
 import axios from "axios"
+import Card from "./card"
 
 
 const Views = () => {
   const [data , setdata]= useState([])
 
   useEffect(()=>{
-    axios.get('http://localhost:8080/view')
+    axios.get('http://localhost:8080/homePage')
     .then((response)=>setdata(response.data));
 
   },[])
-  console.log(data)
   return (
     <>
+    <div className="viewpage-container">
     <Header/>
+    <div className="card-container">
+    <Card data={data}/>
+    </div>
+    </div>
     </>
   )
 }
