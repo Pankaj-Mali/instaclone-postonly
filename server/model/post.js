f2e730210cd39405779133201693d31accd0a8bc
Pankaj-Mali <malipankaj660@gmail.com>
2022-10-04 21:03:10 +0530

    schema update

--- server/model/post.js
+++ server/model/post.js
@@ -6,7 +6,9 @@ const postSchema= new Scheme({
     image:{type:String , required:true},
     name:{type:String , required:true},
     location:{type:String , required:true},
-    description:{type:String , required:true}
+    description:{type:String , required:true},
+    likes:{type:Number},
+    date:{type:String}
 })
 
 const Post = mongoose.model("Post", postSchema)
